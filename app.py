93b326d893e4c5a15ede0ba4b96cf5f20d1f891e
ReshmaRegijc <reshmaregi.jc@gmail.com>
2021-10-07 21:52:28 +0530

    google authentication

--- app.py
+++ app.py
@@ -115,5 +115,6 @@ def login():
 
 
 if __name__=='__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True,host="0.0.0.0", port = port)
+    #port = int(os.environ.get('PORT', 5000))
+    #,host="0.0.0.0", port = port
+    app.run(debug=True)
